13bccd175c71048a7b09646a1d1210b3e43d4cb1
Xinyue (Luna) <xinyuec57@gmail.com>
2023-03-03 12:29:32 -0500

    - add mutation to update project and employee - validate incoming request

--- packages/server/src/employees/employees.service.ts
+++ packages/server/src/employees/employees.service.ts
@@ -8,6 +8,8 @@ export class EmployeesService {
 
   /**
    * Get all employees
+   *
+   * @return a list of employee
    */
   async getAllEmployees(): Promise<Employee[]> {
     return this.prisma.employee.findMany();
@@ -16,11 +18,27 @@ export class EmployeesService {
   /**
    * Add new Employee
    *
-   * @param new employee information details
+   * @param newEmployee new employee information details
    * @returns new employee that has been created
    */
 
   async addEmployee(newEmployee: Prisma.EmployeeCreateInput): Promise<Employee> {
     return this.prisma.employee.create({ data: newEmployee });
+  }
+
+  /**
+   * Update exists employee
+   *
+   * @return The updated employee
+   * @param updateEmployee new updated employee data
+   */
+
+  async updateEmployee(updateEmployee: Prisma.EmployeeUpdateInput): Promise<Employee> {
+    return this.prisma.employee.update({
+      where: {
+        id: updateEmployee.id as string
+      },
+      data: updateEmployee
+    });
   }
 }
--- packages/server/src/project/project.resolver.ts
+++ packages/server/src/project/project.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ProjectService } from './project.service';
 import { ProjectModel } from './model/project.model';
-import { ProjectCreateInput } from './dto/project.dto';
+import { ProjectCreateInput, ProjectUpdateInput } from './dto/project.dto';
 
 @Resolver(() => ProjectModel)
 export class ProjectResolver {
@@ -15,5 +15,10 @@ export class ProjectResolver {
   @Mutation(() => ProjectModel)
   async addProject(@Args('project') project: ProjectCreateInput): Promise<ProjectModel> {
     return this.projectService.addProject(project);
+  }
+
+  @Mutation(() => ProjectModel)
+  async updateProject(@Args('updateProject') updateProject: ProjectUpdateInput): Promise<ProjectModel> {
+    return this.projectService.updateProject(updateProject);
   }
 }
