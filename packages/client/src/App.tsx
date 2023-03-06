8cd5e556cb50b073308c0dc64d0ddaf68a238ec1
xinyue <xinyuec57@gmail.com>
2023-03-06 11:23:28 -0500

    app.tsx

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,7 @@
-import { Track } from '@pages/Track/track';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import Container from '@mui/material/Container';
+import { Track } from '@pages/track';
 
 function App() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Container maxWidth="lg">
-        <Track />
-      </Container>
-    </LocalizationProvider>
-  );
+  return <Track />;
 }
 
 export default App;
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Container from '@mui/material/Container';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Container maxWidth="lg">
+        <App />
+      </Container>
+    </LocalizationProvider>
   </React.StrictMode>
 );
