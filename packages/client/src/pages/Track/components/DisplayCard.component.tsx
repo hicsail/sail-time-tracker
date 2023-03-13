4ae08d451340112a6fd991ae9f70c3621e460d4d
xinyue <xinyuec57@gmail.com>
2023-03-13 11:34:37 -0400

    style: check formatting

--- packages/client/src/pages/Track/components/DisplayCard.component.tsx
+++ packages/client/src/pages/Track/components/DisplayCard.component.tsx
@@ -14,9 +14,10 @@ interface DisplayCardProps {
 
 export const DisplayCard: FC<DisplayCardProps> = ({id, title, hours}) => {
   return (
-    <Card sx={{ maxWidth: 345, maxHeight: 90, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-      <Box paddingLeft="1rem">{id === 'work' ? <WorkSuitIcon width={50} height={50} /> : <RestIcon width={50} height={50} />}</Box>
-      <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+    <Card sx={{maxWidth: 345, maxHeight: 90, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+      <Box paddingLeft="1rem">{id === 'work' ? <WorkSuitIcon width={50} height={50}/> :
+        <RestIcon width={50} height={50}/>}</Box>
+      <CardContent sx={{display: 'flex', flexDirection: 'column', alignItems: 'start'}}>
         <Typography gutterBottom variant="subtitle2" component="div" color="#616161">
           {title}
         </Typography>
