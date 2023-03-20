f2051bc436dc6d6c6bd878edcb4fee293099db9f
xinyue <xinyuec57@gmail.com>
2023-03-20 10:55:35 -0400

    styles: prettier format

--- packages/client/src/pages/Employee/employee.tsx
+++ packages/client/src/pages/Employee/employee.tsx
@@ -8,7 +8,7 @@ export const Employee = () => {
   if (loading || !data) return <div>Loading...</div>;
 
   return (
-    <Box component="div" maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', gap: '4rem', alignItems: 'start' }}>
+    <Box component="div" maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', gap: '4rem', alignItems: 'start', width: '100%' }}>
       {error ? <pre>{error.message}</pre> : <EmployeeTable rows={data.employees} />}
     </Box>
   );
