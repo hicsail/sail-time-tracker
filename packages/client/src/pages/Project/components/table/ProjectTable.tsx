abcd8de25bfdd9d6f554546db21de6a5d619c607
Xinyue (Luna) <56839374+Summer-luna@users.noreply.github.com>
2023-03-27 13:51:29 -0400

    fix: prettier format

--- packages/client/src/pages/Project/components/table/ProjectTable.tsx
+++ packages/client/src/pages/Project/components/table/ProjectTable.tsx
@@ -145,7 +145,7 @@ export const ProjectTable: FC<ProjectTableProps> = ({ rows }) => {
           </Table>
         </TableContainer>
         {rows.length == 0 && (
-          <Button sx={{ width: '100%', height: '200px', fontSize: '1.2rem' }} onClick={() => navigate(Paths.ADD_PROJECT)>
+          <Button sx={{ width: '100%', height: '200px', fontSize: '1.2rem' }} onClick={() => navigate(Paths.ADD_PROJECT)}>
             Add Your First Project
           </Button>
         )}
