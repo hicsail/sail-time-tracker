930a01692c33044d91365a3192790f8c7b748678
Xinyue (Luna) <56839374+Summer-luna@users.noreply.github.com>
2023-04-26 17:56:52 -0400

    refactor: refactor dropdown menu and Date picker component

--- packages/client/src/pages/Track/track.tsx
+++ packages/client/src/pages/Track/track.tsx
@@ -1,22 +1,22 @@
 import { DisplayCard } from '@pages/Track/components/DisplayCard.component';
 import { ProjectTable } from '@pages/Track/components/table/ProjectTable';
-import { DatePickerComponent } from '@pages/Track/components/Date.component';
 
-import { Box, Stack, MenuItem, Select, SelectChangeEvent, InputLabel, FormControl, Container } from '@mui/material';
+import { Box, Stack, SelectChangeEvent } from '@mui/material';
 import { useEmployee } from '@context/employee.context';
 import { useGetEmployeeListQuery, useGetRecordWithFavoriteProjectQuery } from '@graphql/employee/employee';
 import { useDate } from '@context/date.context';
 import { startOfWeek } from 'date-fns';
 import { useEffect } from 'react';
+import TextField from "@mui/material/TextField";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DropDownMenu } from "@pages/Track/components/DropDownMenu";
 
 export const Track = () => {
   const { employeeId, setEmployeeId } = useEmployee();
-  const { date } = useDate();
-  const { data: employeeListData, loading: employeeListLoading, error: employeeListError } = useGetEmployeeListQuery();
+  const { date, setDate } = useDate();
+  const { data: employeeListData } = useGetEmployeeListQuery();
   const {
     data: employeeData,
-    loading: employeeLoading,
-    error: employeeError,
     refetch: refechEmployeeData
   } = useGetRecordWithFavoriteProjectQuery({
     variables: {
@@ -25,7 +25,7 @@ export const Track = () => {
     }
   });
 
-  // refetch records with favorite projects after change the date.
+  // re-fetch records with favorite projects after change the date.
   useEffect(() => {
     refechEmployeeData({
       id: employeeId as string,
@@ -43,9 +43,17 @@ export const Track = () => {
     })
     .reduce((sum, currentProject) => sum + currentProject.hours, 0);
 
-  const changeHandler = (e: SelectChangeEvent) => {
+  /**
+   * employee dropdown change handler
+   * @param e
+   */
+  const employeeChangeHandler = (e: SelectChangeEvent) => {
     setEmployeeId(e.target.value);
   };
+
+  const employees = employeeListData?.employees.map(employee =>{
+    return { id: employee.id, name: employee.name }
+  });
 
   return (
     <Box
@@ -60,25 +68,15 @@ export const Track = () => {
       }}
     >
       <Stack direction="row" spacing={10} sx={{ alignItems: 'center' }}>
-        {employeeListError && <Container>`Fetching error! ${employeeListError.message}`</Container>}
-        {employeeListLoading || !employeeListData ? (
-          <div>Loading...</div>
-        ) : (
-          <FormControl sx={{ minWidth: '200px' }}>
-            <InputLabel id="employee_select-label">Select Employee</InputLabel>
-            <Select name="select_employee" label="Select Employee" id="select_employee" onChange={changeHandler} value={employeeId ? employeeId : ''}>
-              {employeeListData &&
-                employeeListData.employees.map((employee: any) => {
-                  return (
-                    <MenuItem key={employee.id} value={employee.id}>
-                      {employee.name}
-                    </MenuItem>
-                  );
-                })}
-            </Select>
-          </FormControl>
-        )}
-        <DatePickerComponent />
+        <DropDownMenu data={employees} onChange={employeeChangeHandler} label="Select Employee" defaultValue={employeeId} id="select_employee" name="select_employee"/>
+        <DatePicker
+          label="Date"
+          value={date}
+          onChange={(newValue) => {
+            setDate(newValue as Date);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
         <DisplayCard key="work" id="work" title="Total Work Hours" hours={totalWorkHours ? totalWorkHours : 0} />
         <DisplayCard key="absence" id="absence" title="Total Absence Hours" hours={absenceRecord ? absenceRecord[0].hours : 0} />
       </Stack>
