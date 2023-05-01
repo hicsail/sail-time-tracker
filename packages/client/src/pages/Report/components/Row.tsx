f72daac58edd9308c96f013450989bd3608d08b9
Summer-luna <xinyuec57@gmail.com>
2023-05-01 16:09:07 -0400

    feat: create row of collapsible table

---
+++ packages/client/src/pages/Report/components/Row.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import Collapse from '@mui/material/Collapse';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+
+export const Row = (props: { row: any; rowsValue: any; innerHeader: any; innerTitle: string }) => {
+  const { row, rowsValue, innerHeader, innerTitle } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
+          </IconButton>
+        </TableCell>
+        {rowsValue.outer.map((config: any) => {
+          return <TableCell>{config.value(row)}</TableCell>;
+        })}
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                {innerTitle}
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    {innerHeader.map((cell: any) => (
+                      <TableCell align={cell.align}>{cell.name}</TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.inner.map((historyRow: any) => {
+                    return (
+                      <TableRow>
+                        {rowsValue.inner.map((row: any) => {
+                          return <TableCell align={row.align ? row.align : 'left'}>{row.value(historyRow)}</TableCell>;
+                        })}
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+};
