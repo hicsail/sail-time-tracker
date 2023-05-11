0f00b6f4053c6b5db438a0f002c253b773b5f6da
Xinyue (Luna) <xinyuec57@gmail.com>
2023-05-11 09:55:42 -0400

    feat: set start week from Monday

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -14,10 +14,16 @@ import { EmployeeProvider } from '@context/employee.context';
 import { DateProvider } from '@context/date.context';
 import { TrackLayout } from '@pages/Track/components/Layout';
 import { Report } from '@pages/Report/Report';
+import enLocale from 'date-fns/locale/en-US';
 
 function App() {
+
+  if (enLocale && enLocale.options) {
+    enLocale.options.weekStartsOn = 1;
+  }
+
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enLocale}>
       <GraphqlProvider>
         <SettingsProvider>
           <ThemeProvider>
