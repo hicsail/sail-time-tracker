f2d865b102c6499018b58053b05350753e75cc51
Xinyue (Luna) <xinyuec57@gmail.com>
2023-05-18 16:00:36 -0400

    refactor: adjust font-size

--- packages/client/src/pages/Report/components/table/Row.tsx
+++ packages/client/src/pages/Report/components/table/Row.tsx
@@ -31,7 +31,7 @@ export const Row = (props: { row: any; innerTableConfig: any; innerTitle: string
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
+              <Typography gutterBottom component="div" fontWeight="medium">
                 {innerTitle}
               </Typography>
               <Table size="small" aria-label="purchases">
