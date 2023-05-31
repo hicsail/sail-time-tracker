eb73ffa73c30d10fbf12d40dfaa8592bde8c1d0e
Xinyue (Luna) <56839374+Summer-luna@users.noreply.github.com>
2023-05-31 12:58:07 -0400

    Fractional part (#50)
    * feat: format fractional hours
    
    * feat: format fractional hours in GroupByEmployee
    
    * feat: render percentage
    
    * feat: format fractional hours in GroupByProject
    
    * feat: format percentage

--- packages/client/src/pages/Report/GroupByEmployee.tsx
+++ packages/client/src/pages/Report/GroupByEmployee.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { useGetEmployeeListWithRecordQuery } from '@graphql/employee/employee';
 import { startOfWeek } from 'date-fns';
 import { FC } from 'react';
+import { formatHours, formatPercentage } from '../../utils/formatHours';
 
 interface GroupByEmployeeProps {
   date: Date;
@@ -34,17 +35,17 @@ export const GroupByEmployee: FC<GroupByEmployeeProps> = ({ date }) => {
               id: record.project.id,
               name: record.project.name,
               isBillable: record.project.isBillable,
-              workHours: record.hours,
-              indirectHours: Math.round(indirectHour),
-              percentage: Math.round((indirectHour / totalIndirectHours) * 100) + '%'
+              workHours: formatHours(record.hours),
+              indirectHours: formatHours(indirectHour),
+              percentage: formatPercentage((record.hours / totalWorkHours) * 100)
             };
           });
 
         return {
           name: employee.name,
-          workHours: totalWorkHours,
-          indirectHours: totalIndirectHours,
-          billableHours: totalWorkHours + totalIndirectHours,
+          workHours: formatHours(totalWorkHours),
+          indirectHours: formatHours(totalIndirectHours),
+          billableHours: formatHours(totalWorkHours + totalIndirectHours),
           inner: inner
         };
       })
@@ -102,7 +103,7 @@ export const GroupByEmployee: FC<GroupByEmployeeProps> = ({ date }) => {
     },
     {
       name: 'Percentage',
-      render: (row: any) => row.percentage
+      render: (row: any) => row.percentage + '%'
     }
   ];
 
