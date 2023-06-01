6f2187f651cf4d8e77548fc48bdc978d62801012
Xinyue (Luna) <xinyuec57@gmail.com>
2023-05-31 20:30:10 -0400

    refactor: new theme color

--- packages/client/src/theme/theme.light.ts
+++ packages/client/src/theme/theme.light.ts
@@ -10,9 +10,25 @@ export const themeLight = createTheme({
     },
     primary: {
       light: '#757ce8',
-      main: '#737373',
+      main: '#4A4ECC',
+      dark: '#3d40ba',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#B6B3C3',
+      main: '#767575',
       dark: '#262626',
       contrastText: '#fff'
+    },
+    success: {
+      light: '#EAF8F1',
+      main: '#1D7F54',
+      dark: ''
+    },
+    error: {
+      light: '#FFE3EC',
+      main: '#C6325E',
+      dark: ''
     },
     customColors: {
       listNavHoverColor: '#eef2ff',
