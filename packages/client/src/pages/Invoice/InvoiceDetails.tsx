49267a804a44f73f200e2402347df7d024a55f64
Xinyue (Luna) <xinyuec57@gmail.com>
2023-06-03 21:33:28 -0400

    refactor: updated invoice details ui

--- packages/client/src/pages/Invoice/InvoiceDetails.tsx
+++ packages/client/src/pages/Invoice/InvoiceDetails.tsx
@@ -1,107 +1,59 @@
 import { Box, Button } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import { CollapsibleTable } from '@pages/Report/components/table/CollapsibleTable';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const InvoiceDetails = () => {
   const { id } = useParams();
 
-  const rows: any[] = [
+  const columns: GridColDef[] = [
     {
-      name: 'Project 1',
-      scheduledValue: 10000,
-      previousBilling: 3000,
-      currentPeriodWork: 1000,
-      totalCompleted: 4000,
-      remainingBalance: 6000,
-      inner: [
-        {
-          name: 'Employee 1',
-          rate: 45,
-          workHours: 15,
-          gross: 675
-        },
-        {
-          name: 'Employee 2',
-          rate: 50,
-          workHours: 6.5,
-          gross: 325
-        }
-      ]
+      field: 'employeeName',
+      headerName: 'Employee Name',
+      width: 200,
+      renderCell: (params) => params.row.projectName
+    },
+    { field: 'rate', headerName: 'Rate', width: 200 },
+    { field: 'amount', headerName: 'Amount', width: 200 }
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      employeeName: 'Employee 1',
+      rate: 65,
+      amount: 3500
     },
     {
-      name: 'Project 2',
-      scheduledValue: 20000,
-      previousBilling: 3000,
-      currentPeriodWork: 1000,
-      totalCompleted: 4000,
-      remainingBalance: 16000,
-      inner: [
-        {
-          name: 'Employee 1',
-          rate: 45,
-          workHours: 15,
-          gross: 675
-        },
-        {
-          name: 'Employee 2',
-          rate: 50,
-          workHours: 6.5,
-          gross: 325
-        }
-      ]
+      id: 2,
+      employeeName: 'Employee 2',
+      rate: 65,
+      amount: 4200
+    },
+    {
+      id: 3,
+      employeeName: 'Employee 3',
+      rate: 65,
+      amount: 4500
+    },
+    {
+      id: 4,
+      employeeName: 'Employee 4',
+      rate: 65,
+      amount: 1600
+    },
+    {
+      id: 5,
+      employeeName: 'Employee 5',
+      rate: 65,
+      amount: 2000
     }
   ];
 
-  // outer table column name and render config
-  const outerTableConfig = [
-    {
-      name: 'Projects',
-      render: (row: any) => row.name
-    },
-    {
-      name: 'Scheduled Value',
-      render: (row: any) => row.scheduledValue
-    },
-    {
-      name: 'Previously Billing',
-      render: (row: any) => row.previousBilling
-    },
-    {
-      name: 'Current Period Work',
-      render: (row: any) => row.currentPeriodWork
-    },
-    {
-      name: 'Total Completed',
-      render: (row: any) => row.totalCompleted
-    },
-    {
-      name: 'Remaining Balance',
-      render: (row: any) => row.remainingBalance
-    }
-  ];
-
-  // inner table column name and render config
-  const innerTableConfig = [
-    {
-      name: 'Name',
-      render: (row: any) => row.name
-    },
-    {
-      name: 'Rate',
-      render: (row: any) => row.rate
-    },
-    {
-      name: 'Work Hours',
-      render: (row: any) => row.workHours
-    },
-    {
-      name: 'Amount',
-      render: (row: any) => row.gross
-    }
-  ];
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItem: 'center', gap: 8 }}>
+    <Box sx={{ width: '100%', height: 400 }}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <Box>
           <h3>{`Invoice: #${id}`}</h3>
@@ -110,14 +62,20 @@ export const InvoiceDetails = () => {
             <div>5/29/2023 - 6/5/2023</div>
           </Box>
         </Box>
-        <Box sx={{ color: 'error.main', fontWeight: 'bold', cursor: 'pointer', textDecoration: 'underline' }}>Delete</Box>
+        <DeleteIcon color="secondary" sx={{ cursor: 'pointer' }} />
       </Box>
-      <Box>
-        <CollapsibleTable rows={rows} outerTableConfig={outerTableConfig} innerTableConfig={innerTableConfig} innerTitle="Employees" />
-      </Box>
-      <Box sx={{ display: 'flex', gap: 3 }}>
-        <Button variant="contained">Export to Clickup</Button>
-      </Box>
+      <DataGrid
+        sx={{ marginTop: 6, color: '#021352' }}
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 }
+          }
+        }}
+        pageSizeOptions={[5, 10]}
+        disableRowSelectionOnClick
+      />
     </Box>
   );
 };
