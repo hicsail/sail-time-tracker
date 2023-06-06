2c50f14f1a7c6fa3da83a97ea37af84ab17cd703
Summer-luna <xinyuec57@gmail.com>
2023-06-06 16:20:09 -0400

    feat: import employee and project module

--- packages/server/src/invoice/invoice.module.ts
+++ packages/server/src/invoice/invoice.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
 import { InvoiceResolver } from './invoice.resolver';
 import { PrismaModule } from 'nestjs-prisma';
+import { EmployeesModule } from '../employees/employees.module';
+import { ProjectModule } from '../project/project.module';
 
 @Module({
-  imports: [PrismaModule.forRoot()],
+  imports: [PrismaModule.forRoot(), EmployeesModule, ProjectModule],
   providers: [InvoiceService, InvoiceResolver]
 })
 export class InvoiceModule {}
