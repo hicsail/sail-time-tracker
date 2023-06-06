258c4869a5108139e100c9d80f47b7429aad2a0a
Summer-luna <xinyuec57@gmail.com>
2023-06-06 13:26:17 -0400

    feat: invoice resolver

---
+++ packages/server/src/invoice/invoice.resolver.ts
@@ -0,0 +1,28 @@
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
+import {InvoiceService} from "./invoice.service";
+import {InvoiceModel} from "./model/invoice.model";
+import {InvoiceCreateInput} from "./dto/invoice.dto";
+import {BatchPayload} from "../favorite-project/model/favorite-project.model";
+
+
+@Resolver()
+export class InvoiceResolver {
+  constructor(private invoiceService: InvoiceService) {
+  }
+
+  @Query(() => [InvoiceModel])
+  async invoices(): Promise<InvoiceModel[]> {
+    return this.invoiceService.getAllInvoices();
+  }
+
+  @Mutation(() => BatchPayload)
+  async createManyInvoice(
+    @Args({
+      name: 'invoices',
+      type: () => [InvoiceCreateInput]
+    })
+      invoices: [InvoiceCreateInput]
+  ): Promise<BatchPayload> {
+    return this.invoiceService.createManyInvoice(invoices);
+  }
+}
