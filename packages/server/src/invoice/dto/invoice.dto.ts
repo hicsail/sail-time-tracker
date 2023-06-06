37b711cfa6d31a794b5f7d01056078e901c17697
Summer-luna <xinyuec57@gmail.com>
2023-06-06 13:27:34 -0400

    feat: invoice dto

---
+++ packages/server/src/invoice/dto/invoice.dto.ts
@@ -0,0 +1,56 @@
+import { InputType, Field, ID } from '@nestjs/graphql';
+import {IsEmail, IsNotEmpty, IsString, IsOptional, IsNumber, IsDate, isNumber} from 'class-validator';
+
+/**
+ * input type for creating new invoice
+ */
+
+@InputType()
+export class InvoiceCreateInput {
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  projectId: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  employeeId: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsDate()
+  startDate: Date;
+
+  @Field()
+  @IsNotEmpty()
+  @IsDate()
+  endDate: Date;
+
+  @Field()
+  @IsNotEmpty()
+  @IsNumber()
+  rate: number;
+
+  @Field()
+  @IsNotEmpty()
+  @IsNumber()
+  hours: number;
+
+  @Field()
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+}
+
+/**
+ * input type for updating invoice
+ */
+
+@InputType()
+export class InvoiceUpdateInput extends InvoiceCreateInput {
+  @Field(() => ID)
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+}
