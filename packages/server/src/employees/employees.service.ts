585efe297a349443096b0cf7fa5e0be7c8e90fbf
Xinyue (Luna) <xinyuec57@gmail.com>
2023-06-16 10:18:59 -0400

    fix: update input types

--- packages/server/src/employees/employees.service.ts
+++ packages/server/src/employees/employees.service.ts
@@ -6,6 +6,7 @@ import { ProjectModel } from '../project/model/project.model';
 import { previousDay } from 'date-fns';
 import { RecordWithFavoriteProjectModel } from '../record/model/record.model';
 import { formatHours, formatPercentage } from '../utils/helperFun';
+import { EmployeeCreateInput, EmployeeUpdateInput } from './dto/employee.dto';
 
 @Injectable()
 export class EmployeesService {
@@ -40,7 +41,7 @@ export class EmployeesService {
    * @returns new employee that has been created
    */
 
-  async addEmployee(newEmployee: Prisma.EmployeeCreateInput): Promise<Employee> {
+  async addEmployee(newEmployee: EmployeeCreateInput): Promise<Employee> {
     return this.prisma.employee.create({ data: newEmployee });
   }
 
@@ -51,7 +52,7 @@ export class EmployeesService {
    * @param updateEmployee new updated employee data
    */
 
-  async updateEmployee(updateEmployee: Prisma.EmployeeUpdateInput): Promise<Employee> {
+  async updateEmployee(updateEmployee: EmployeeUpdateInput): Promise<Employee> {
     return this.prisma.employee.update({
       where: {
         id: updateEmployee.id as string
