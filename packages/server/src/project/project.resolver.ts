db913015467c910c698f509b0bcc0716f3d6b738
Summer-luna <xinyuec57@gmail.com>
2023-06-27 14:38:52 -0400

    refactor: remove unused project model

--- packages/server/src/project/project.resolver.ts
+++ packages/server/src/project/project.resolver.ts
@@ -1,6 +1,6 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ProjectService } from './project.service';
-import { ProjectDeleteReturnModel, ProjectModel, ProjectWithRecord } from './model/project.model';
+import { ProjectDeleteReturnModel, ProjectModel } from './model/project.model';
 import { ProjectCreateInput, ProjectUpdateInput } from './dto/project.dto';
 
 @Resolver(() => ProjectModel)
