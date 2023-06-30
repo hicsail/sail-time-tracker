17e232047ebdb8e0f69247b8e575032575711377
Greg Frasco <frascog17@gmail.com>
2023-06-30 13:57:50 -0400

    fix: add cors here

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https:test-time.sail.codes', 'https://time.sail.codes'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: true
+  })
   await app.listen(3000);
 }
 
