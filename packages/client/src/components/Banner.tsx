9f0d42679b0bb3a4931b4cb9a4b745fa721a4de2
Summer-luna <xinyuec57@gmail.com>
2023-07-05 17:03:13 -0400

    fix: handle info state

--- packages/client/src/components/Banner.tsx
+++ packages/client/src/components/Banner.tsx
@@ -4,21 +4,61 @@ import Typography from '@mui/material/Typography';
 import { FC } from 'react';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+import InfoIcon from '@mui/icons-material/Info';
 import { Box } from '@mui/material';
 
 interface BannerProps {
   content: string;
-  state: 'success' | 'error';
+  state: 'success' | 'error' | 'info';
 }
 
 export const Banner: FC<BannerProps> = ({ content, state }) => {
+  const contentRender = () => {
+    switch (state) {
+      case 'success':
+        return <CheckCircleOutlineIcon color="success" fontSize="large" />;
+      case 'error':
+        return <ReportGmailerrorredIcon color="error" fontSize="large" />;
+      case 'info':
+        return <InfoIcon color="info" fontSize="large" />;
+      default:
+        return <InfoIcon color="info" fontSize="large" />;
+    }
+  };
+
+  const messageRender = () => {
+    switch (state) {
+      case 'success':
+        return 'Success!';
+      case 'error':
+        return 'Something Went Wrong!';
+      case 'info':
+        return 'Info!';
+      default:
+        return <InfoIcon color="info" fontSize="large" />;
+    }
+  };
+
+  const colorRender = () => {
+    switch (state) {
+      case 'success':
+        return 'success.dark';
+      case 'error':
+        return 'error.dark';
+      case 'info':
+        return 'info.dark';
+      default:
+        return 'info.dark';
+    }
+  };
+
   return (
-    <Card sx={{ position: 'fixed', top: '5%', right: '6%', transform: 'translate(-50%,-50%)', width: 'auto', padding: '3px 10px', zIndex: 10 }}>
+    <Card sx={{ position: 'fixed', top: '8%', right: '35%', transform: 'translate(-50%,-50%)', width: 'auto', padding: '3px 10px', zIndex: 10 }}>
       <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <CardContent>{state === 'success' ? <CheckCircleOutlineIcon color="success" fontSize="large" /> : <ReportGmailerrorredIcon color="error" fontSize="large" />}</CardContent>
+        <CardContent>{contentRender()}</CardContent>
         <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="subtitle1" sx={{ fontWeight: 'bold', color: `${state === 'success' ? 'success.dark' : 'error.dark'}` }}>
-            {state === 'success' ? 'Success!' : 'Something Went Wrong!'}
+          <Typography component="div" variant="subtitle1" sx={{ fontWeight: 'bold', color: `${colorRender()}` }}>
+            {messageRender()}
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" component="div">
             {content}
