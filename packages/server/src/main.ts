e49ba66eadcbafc188705d0e110dc52c6e94eaa9
Greg Frasco <frascog17@gmail.com>
2023-07-10 11:56:24 -0400

    feat: cors

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,16 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['http://localhost:5173', 'https://*.sail.codes'],
-      preflightContinue: true,
-      credentials: true,
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      optionsSuccessStatus: 204
-    }
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
