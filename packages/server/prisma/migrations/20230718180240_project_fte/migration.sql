6f7898847f88ebe25a687a658a06b505b3809026
Summer-luna <xinyuec57@gmail.com>
2023-07-18 14:24:02 -0400

    fix: set project fte default to 0

--- packages/server/prisma/migrations/20230718180240_project_fte/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "fte" DOUBLE PRECISION;
---
+++ packages/server/prisma/migrations/20230718181717_project_fte/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "fte" DOUBLE PRECISION NOT NULL DEFAULT 0;
