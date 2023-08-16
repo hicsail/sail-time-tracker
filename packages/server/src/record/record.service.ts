e7471ba49c13089a1f27188fb88b272a5a407893
Xinyue (Luna) <xinyuec57@gmail.com>
2023-08-16 12:28:21 -0400

    feat: add comments

--- packages/server/src/record/record.service.ts
+++ packages/server/src/record/record.service.ts
@@ -8,6 +8,11 @@ import { BatchPayload } from '../favorite-project/model/favorite-project.model';
 export class RecordService {
   constructor(private prisma: PrismaService) {}
 
+  /**
+   * add one or more records
+   * @param record: {employeeId:string, projectId:string, hours: number, date: Date}
+   * @returns {count: number}
+   */
   async insertOrUpdateRecord(record: RecordCreateInput): Promise<Record> {
     return this.prisma.record.upsert({
       where: {
@@ -32,6 +37,16 @@ export class RecordService {
     });
   }
 
+  /**
+   * Delete one or more records that have zero hours
+   *
+   * @param employeeId: string
+   * @param projectIds: string[]
+   * @param startDate: Date
+   * @param endDate: Date
+   * @returns {count: number}
+   *
+   */
   async deleteZeroHoursRecord(employeeId: string, projectIds: string[], startDate: Date, endDate: Date): Promise<BatchPayload> {
     return this.prisma.record.deleteMany({
       where: {
