b308f73654eda0fc573c7dfafec0a790cbaff228
Greg Frasco <frascog17@gmail.com>
2023-11-08 13:57:07 -0500

    fix: add Invoice Item migration

---
+++ packages/server/prisma/migrations/20231108185508_invoice_item/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "InvoiceItem" (
+    "invoiceId" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
+    "workHours" DOUBLE PRECISION NOT NULL,
+    "indirectHours" DOUBLE PRECISION NOT NULL,
+    "billableHours" DOUBLE PRECISION NOT NULL,
+    "rate" DOUBLE PRECISION NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "InvoiceItem_pkey" PRIMARY KEY ("invoiceId","employeeId")
+);
+
+-- AddForeignKey
+ALTER TABLE "InvoiceItem" ADD CONSTRAINT "InvoiceItem_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("invoiceId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "InvoiceItem" ADD CONSTRAINT "InvoiceItem_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
